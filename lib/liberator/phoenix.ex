ff4691ecb4c48cffe07e927d544125e5674855ff
Rosa Richter <cosmic.lady.rosa@gmail.com>
2021-09-06 17:21:33 -0600

    Allow view module override

--- lib/liberator/phoenix.ex
+++ lib/liberator/phoenix.ex
@@ -32,6 +32,8 @@ defmodule Liberator.Phoenix do
   The handlers will use the status code as the template name and the extension of the negotiated media type.
   For example, the `Liberator.Resource.handle_not_found/1` handler with a negotiated media type of `text/plain` will render `404.txt`.
   The `Liberator.Resource.handle_created/1` handler with a media type of `application/json` will render `201.json`.
+
+  The values set in `conn.assigns` will be given to the view as well.
   """
 
   def view_name(module) do
@@ -50,9 +52,10 @@ defmodule Liberator.Phoenix do
     end
   end
 
-  def render(conn, template, assigns) do
+  def render(conn, template, opts) do
+    view_module = Keyword.get(opts, :view_module, view_module(conn))
     template_name = template <> "." <> get_format(conn)
-    Phoenix.View.render(view_module(conn), template_name, assigns)
+    Phoenix.View.render(view_module, template_name, conn.assigns)
   end
 
   def view_module(conn) do
@@ -76,142 +79,142 @@ defmodule Liberator.Phoenix do
 
       if handler_enabled?(:handle_ok, opts) do
         @impl Liberator.Resource
-        def handle_ok(conn), do: render(conn, "200", [])
+        def handle_ok(conn), do: render(conn, "200", unquote(opts))
       end
 
       if handler_enabled?(:handle_options, opts) do
         @impl Liberator.Resource
-        def handle_options(conn), do: render(conn, "200-options", [])
+        def handle_options(conn), do: render(conn, "200-options", unquote(opts))
       end
 
       if handler_enabled?(:handle_created, opts) do
         @impl Liberator.Resource
-        def handle_created(conn), do: render(conn, "201", [])
+        def handle_created(conn), do: render(conn, "201", unquote(opts))
       end
 
       if handler_enabled?(:handle_accepted, opts) do
         @impl Liberator.Resource
-        def handle_accepted(conn), do: render(conn, "202", [])
+        def handle_accepted(conn), do: render(conn, "202", unquote(opts))
       end
 
       if handler_enabled?(:handle_no_content, opts) do
         @impl Liberator.Resource
-        def handle_no_content(conn), do: render(conn, "204", [])
+        def handle_no_content(conn), do: render(conn, "204", unquote(opts))
       end
 
       if handler_enabled?(:handle_multiple_representations, opts) do
         @impl Liberator.Resource
-        def handle_multiple_representations(conn), do: render(conn, "300", [])
+        def handle_multiple_representations(conn), do: render(conn, "300", unquote(opts))
       end
 
       if handler_enabled?(:handle_moved_permanently, opts) do
         @impl Liberator.Resource
-        def handle_moved_permanently(conn), do: render(conn, "301", [])
+        def handle_moved_permanently(conn), do: render(conn, "301", unquote(opts))
       end
 
       if handler_enabled?(:handle_see_other, opts) do
         @impl Liberator.Resource
-        def handle_see_other(conn), do: render(conn, "303", [])
+        def handle_see_other(conn), do: render(conn, "303", unquote(opts))
       end
 
       if handler_enabled?(:handle_not_modified, opts) do
         @impl Liberator.Resource
-        def handle_not_modified(conn), do: render(conn, "304", [])
+        def handle_not_modified(conn), do: render(conn, "304", unquote(opts))
       end
 
       if handler_enabled?(:handle_malformed, opts) do
         @impl Liberator.Resource
-        def handle_malformed(conn), do: render(conn, "400", [])
+        def handle_malformed(conn), do: render(conn, "400", unquote(opts))
       end
 
       if handler_enabled?(:handle_payment_required, opts) do
         @impl Liberator.Resource
-        def handle_payment_required(conn), do: render(conn, "402", [])
+        def handle_payment_required(conn), do: render(conn, "402", unquote(opts))
       end
 
       if handler_enabled?(:handle_unauthorized, opts) do
         @impl Liberator.Resource
-        def handle_unauthorized(conn), do: render(conn, "401", [])
+        def handle_unauthorized(conn), do: render(conn, "401", unquote(opts))
       end
 
       if handler_enabled?(:handle_forbidden, opts) do
         @impl Liberator.Resource
-        def handle_forbidden(conn), do: render(conn, "403", [])
+        def handle_forbidden(conn), do: render(conn, "403", unquote(opts))
       end
 
       if handler_enabled?(:handle_not_found, opts) do
         @impl Liberator.Resource
-        def handle_not_found(conn), do: render(conn, "404", [])
+        def handle_not_found(conn), do: render(conn, "404", unquote(opts))
       end
 
       if handler_enabled?(:handle_method_not_allowed, opts) do
         @impl Liberator.Resource
-        def handle_method_not_allowed(conn), do: render(conn, "405", [])
+        def handle_method_not_allowed(conn), do: render(conn, "405", unquote(opts))
       end
 
       if handler_enabled?(:handle_not_acceptable, opts) do
         @impl Liberator.Resource
-        def handle_not_acceptable(conn), do: render(conn, "406", [])
+        def handle_not_acceptable(conn), do: render(conn, "406", unquote(opts))
       end
 
       if handler_enabled?(:handle_conflict, opts) do
         @impl Liberator.Resource
-        def handle_conflict(conn), do: render(conn, "409", [])
+        def handle_conflict(conn), do: render(conn, "409", unquote(opts))
       end
 
       if handler_enabled?(:handle_gone, opts) do
         @impl Liberator.Resource
-        def handle_gone(conn), do: render(conn, "410", [])
+        def handle_gone(conn), do: render(conn, "410", unquote(opts))
       end
 
       if handler_enabled?(:handle_precondition_failed, opts) do
         @impl Liberator.Resource
-        def handle_precondition_failed(conn), do: render(conn, "412", [])
+        def handle_precondition_failed(conn), do: render(conn, "412", unquote(opts))
       end
 
       if handler_enabled?(:handle_request_entity_too_large, opts) do
         @impl Liberator.Resource
-        def handle_request_entity_too_large(conn), do: render(conn, "413", [])
+        def handle_request_entity_too_large(conn), do: render(conn, "413", unquote(opts))
       end
 
       if handler_enabled?(:handle_uri_too_long, opts) do
         @impl Liberator.Resource
-        def handle_uri_too_long(conn), do: render(conn, "414", [])
+        def handle_uri_too_long(conn), do: render(conn, "414", unquote(opts))
       end
 
       if handler_enabled?(:handle_unsupported_media_type, opts) do
         @impl Liberator.Resource
-        def handle_unsupported_media_type(conn), do: render(conn, "415", [])
+        def handle_unsupported_media_type(conn), do: render(conn, "415", unquote(opts))
       end
 
       if handler_enabled?(:handle_too_many_requests, opts) do
         @impl Liberator.Resource
-        def handle_too_many_requests(conn), do: render(conn, "429", [])
+        def handle_too_many_requests(conn), do: render(conn, "429", unquote(opts))
       end
 
       if handler_enabled?(:handle_unavailable_for_legal_reasons, opts) do
         @impl Liberator.Resource
-        def handle_unavailable_for_legal_reasons(conn), do: render(conn, "451", [])
+        def handle_unavailable_for_legal_reasons(conn), do: render(conn, "451", unquote(opts))
       end
 
       if handler_enabled?(:handle_error, opts) do
         @impl Liberator.Resource
-        def handle_error(conn, _error, _failed_step), do: render(conn, "500", [])
+        def handle_error(conn, _error, _failed_step), do: render(conn, "500", unquote(opts))
       end
 
       if handler_enabled?(:handle_not_implemented, opts) do
         @impl Liberator.Resource
-        def handle_not_implemented(conn), do: render(conn, "501", [])
+        def handle_not_implemented(conn), do: render(conn, "501", unquote(opts))
       end
 
       if handler_enabled?(:handle_unknown_method, opts) do
         @impl Liberator.Resource
-        def handle_unknown_method(conn), do: render(conn, "501", [])
+        def handle_unknown_method(conn), do: render(conn, "501", unquote(opts))
       end
 
       if handler_enabled?(:handle_service_unavailable, opts) do
         @impl Liberator.Resource
-        def handle_service_unavailable(conn), do: render(conn, "503", [])
+        def handle_service_unavailable(conn), do: render(conn, "503", unquote(opts))
       end
     end
   end
--- test/liberator/phoenix_test.exs
+++ test/liberator/phoenix_test.exs
@@ -3,16 +3,16 @@ defmodule Liberator.PhoenixTest do
   use Plug.Test
   doctest Liberator.Phoenix
 
+  defmodule HappyPathResource do
+    use Liberator.Resource
+    use Liberator.Phoenix
+  end
+
+  defmodule HappyPathView do
+    use Phoenix.View, root: "test/support/templates"
+  end
+
   test "handler returns result of view" do
-    defmodule HappyPathResource do
-      use Liberator.Resource
-      use Liberator.Phoenix
-    end
-
-    defmodule HappyPathView do
-      use Phoenix.View, root: "test/support/templates"
-    end
-
     conn = conn(:get, "/")
 
     conn = HappyPathResource.call(conn, [])
@@ -20,16 +20,16 @@ defmodule Liberator.PhoenixTest do
     assert conn.resp_body == "OK (rendered from template)\n\n"
   end
 
+  defmodule ExceptResource do
+    use Liberator.Resource
+    use Liberator.Phoenix, except: [:handle_ok]
+  end
+
+  defmodule ExceptView do
+    use Phoenix.View, root: "test/support/templates"
+  end
+
   test "handler can be excluded with :except option" do
-    defmodule ExceptResource do
-      use Liberator.Resource
-      use Liberator.Phoenix, except: [:handle_ok]
-    end
-
-    defmodule ExceptView do
-      use Phoenix.View, root: "test/support/templates"
-    end
-
     conn = conn(:get, "/")
 
     conn = ExceptResource.call(conn, [])
@@ -37,19 +37,36 @@ defmodule Liberator.PhoenixTest do
     assert conn.resp_body == "OK"
   end
 
+  defmodule OnlyResource do
+    use Liberator.Resource
+    use Liberator.Phoenix, only: [:handle_ok]
+  end
+
+  defmodule OnlyView do
+    use Phoenix.View, root: "test/support/templates"
+  end
+
   test "handler can be include with :only option" do
-    defmodule OnlyResource do
-      use Liberator.Resource
-      use Liberator.Phoenix, only: [:handle_ok]
-    end
-
-    defmodule OnlyView do
-      use Phoenix.View, root: "test/support/templates"
-    end
-
     conn = conn(:get, "/")
 
     conn = OnlyResource.call(conn, [])
+
+    assert conn.resp_body == "OK (rendered from template)\n\n"
+  end
+
+  defmodule OtherModuleResource do
+    use Liberator.Resource, trace: :log
+    use Liberator.Phoenix, view_module: Liberator.PhoenixTest.AnotherModule
+  end
+
+  defmodule AnotherModule do
+    use Phoenix.View, root: "test/support/templates"
+  end
+
+  test "A different view module can be specified with the :view_module option" do
+    conn = conn(:get, "/")
+
+    conn = OtherModuleResource.call(conn, [])
 
     assert conn.resp_body == "OK (rendered from template)\n\n"
   end
